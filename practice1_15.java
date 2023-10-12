7f8165eeb94e2a9bce3597674ee327d019b0cf2f
alarconvv <alarconvv@gmail.com>
2023-10-12 17:32:49 -0400

    231012

---
+++ practice1_15.java
@@ -0,0 +1,13 @@
+// Write a while statement that is equivalent to the following for fragment 
+//for(;;)statent
+
+public class practice1_15{
+    public static void main(String[] arg){
+       int x = 0;
+        while( x != 7){
+            System.out.print(x);
+             x = ++x;
+        }
+
+    }
+}
